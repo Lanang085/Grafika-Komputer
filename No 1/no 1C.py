42eac7e358c72269a80a7feb754a7b5154abb75e
Lanang085 <72660464+Lanang085@users.noreply.github.com>
2022-03-16 01:12:48 +0700

    Add files via upload

---
+++ No 1/no 1C.py
@@ -0,0 +1,38 @@
+# Nama  : M Gigih Lanang P
+# NIM   : 20051397053
+# Kelas : D4 Manajamen Informatika 2020A
+
+
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-6, 6, -6, 6, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glColor3f(1, 1, 1)
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(0.0, 0.00)
+    glVertex2f(2, 3)
+    glVertex2f(0, 0)
+    glVertex2f(5, 3)
+    glEnd()
+
+    glutSwapBuffers()
+
+
+glutInit(sys.argv)
+glutInitWindowSize(500, 500)
+glutCreateWindow("1C")
+glutDisplayFunc(draw)
+glutMainLoop()
